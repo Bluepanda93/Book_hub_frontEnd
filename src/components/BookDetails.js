ad7df16b05f06436e5381888b2b72026be270605
Daniel Shull <danielshull121@gmail.com>
2022-11-15 11:46:36 -0500

    getting rid of unneccassary things

---
+++ src/components/BookDetails.js
@@ -0,0 +1,13 @@
+// import Books from './pages/BooksPage'
+// import axios from 'axios'
+// import { Navigate, useNavigate, useParams } from 'react-router-dom'
+// import { useEffect } from 'react'
+
+const BookDetails = () => {
+    return (
+        <div>details</div>
+    )
+
+
+}
+export default BookDetails
