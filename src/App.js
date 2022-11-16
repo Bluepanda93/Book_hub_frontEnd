b0fcaf2f8ca2368cd36f631aa715035668f3ee15
Philip Basnight <philipbasnight@gmail.com>
2022-11-16 11:38:59 -0500

    gourds of coding

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
@@ -5,21 +6,66 @@ import Home from './pages/Home'
 import Books from './pages/BooksPage'
 import CreateBook from './pages/CreateBook'
 import BookDetails from './components/BookDetails'
+import Register from './pages/Register'
+import { CheckSession } from './services/Auth'
+import Login from './pages/Login'
 
 const App = () => {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    //If a token exists, sends token to localStorage to persist logged in user
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    // Check if token exists before requesting to validate the token
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div>
       <div className="page">
         <header>
-          <Navbar />
+          <Navbar
+            authenticated={authenticated}
+            user={user}
+            handleLogOut={handleLogOut}
+          />
         </header>
       </div>
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/login"
+            element={
+              <Login
+                setUser={setUser}
+                toggleAuthenticated={toggleAuthenticated}
+              />
+            }
+          />
           <Route path="/add_book" element={<CreateBook />} />
-          <Route path="/books" element={<Books />} />
-          <Route path='books/:id' element={<BookDetails />} />
+          <Route
+            path="/books"
+            element={<Books user={user} authenticated={authenticated} />}
+          />
+          <Route path="books/:id" element={<BookDetails />} />
         </Routes>
       </div>
     </div>
--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -2,44 +2,48 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
-const Books = () => {
-    const [books, setBooks] = useState([])
+const Books = ({ user, authenticated }) => {
+  const [books, setBooks] = useState([])
 
-    const getBooks = async () => {
-        try {
-            let response = await axios.get('https://the-book-hub-generalassembly.herokuapp.com/api/books/all_books')
-            console.log(response.data)
-            setBooks(response.data)
-        } catch (error) {
-            console.log(error)
-        }
-
+  const getBooks = async () => {
+    try {
+      let response = await axios.get(
+        'https://the-book-hub-generalassembly.herokuapp.com/api/books/all_books'
+      )
+      console.log(response.data)
+      setBooks(response.data)
+    } catch (error) {
+      console.log(error)
     }
-    useEffect(() => {
-        getBooks()
-    }, [])
-    let navigate = useNavigate()
+  }
+  useEffect(() => {
+    getBooks()
+  }, [])
+  let navigate = useNavigate()
 
-    const getBookDetails = (id) => {
-        navigate(`${id}`)
-    }
+  const getBookDetails = (id) => {
+    navigate(`${id}`)
+  }
 
-    return (
-        <div>
-            <h1>This is the books page</h1>
-            <div className="booksHolder">
-                {books.map((book) => (
-                    <div key={book.id} onClick={() => getBookDetails(book.id)}>
-                        <h2>{book.title}</h2>
-                        <h3>{book.author}</h3>
-                        <h3>{book.genre}</h3>
-                    </div>
-                ))}
-            </div>
-        </div>
-
-
-    )
+  return user && authenticated ? (
+    <div>
+      <h1>This is the books page</h1>
+      <div className="booksHolder">
+        {books.map((book) => (
+          <div key={book.id} onClick={() => getBookDetails(book.id)}>
+            <h2>{book.title}</h2>
+            <h3>{book.author}</h3>
+            <h3>{book.genre}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="protected">
+      <h3>Oops! You must be signed in to do that!</h3>
+      <button onClick={() => navigate('/login')}>Sign In</button>
+    </div>
+  )
 }
 
 export default Books
